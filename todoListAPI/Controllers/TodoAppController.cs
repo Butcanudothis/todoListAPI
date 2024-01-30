1f1a887dca93445e482e34f12fcfcd8567b01c4f
butcanudothis <akshayvarma.varma@gmail.com>
2024-01-30 18:56:36 +1000

    added get notes method

---
+++ todoListAPI/Controllers/TodoAppController.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using System.Data;
+using System.Security.AccessControl;
+using System.Data.SqlClient;
+namespace todoListAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TodoAppController : ControllerBase
+    {
+        private IConfiguration _config;
+        public TodoAppController(IConfiguration config)
+        {
+            _config = config;
+        }
+        [HttpGet]
+        [Route("GetNotes")]
+        public JsonResult GetNotes()
+        {
+            string query = "select * from dbo.Notes";
+            DataTable table = new DataTable();
+            string sqlDataSource = _config.GetConnectionString("todoAppDBCon");
+            SqlDataReader myReader;
+            using (SqlConnection myCon = new SqlConnection(sqlDataSource))
+            {
+                myCon.Open();
+                using (SqlCommand myCommand = new SqlCommand(query, myCon))
+                {
+                    myReader = myCommand.ExecuteReader();
+                    table.Load(myReader);
+                    myReader.Close();
+                    myCon.Close();
+                }
+            } 
+            return new JsonResult(table);
+        }
+    }
+}
